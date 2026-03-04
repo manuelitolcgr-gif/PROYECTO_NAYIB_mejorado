e6c2b45a4e7a2494e64a26d6b5f3525cd12b30cf
manuelitolcgr-gif <manuelitolcgr@gmail.com>
2026-03-03 19:11:50 -0600

    Translate booking status and display percentage table

--- Grafico_proyecto_nayib.py
+++ Grafico_proyecto_nayib.py
@@ -1,10 +1,25 @@
 st.subheader("Estado de los Viajes")
 
 status_count = filtered_df['booking_status'].value_counts()
+
 status_percent = (status_count / status_count.sum()) * 100
+
+status_count.index = status_count.index.map({
+    "Completed": "Completado",
+    "Incomplete": "Incompleto",
+    "Canceled": "Cancelado"
+})
+
+status_percent.index = status_percent.index.map({
+    "Completed": "Completado",
+    "Incomplete": "Incompleto",
+    "Canceled": "Cancelado"
+})
 
 st.bar_chart(status_count)
 
-st.write("Porcentaje de cada estado:")
-st.write(status_percent.round(2))
+tabla_porcentaje = status_percent.round(2).reset_index()
+tabla_porcentaje.columns = ["Estado del Viaje", "Porcentaje (%)"]
 
+st.write("### Porcentaje de cada estado")
+st.dataframe(tabla_porcentaje)
